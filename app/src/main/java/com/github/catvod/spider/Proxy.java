c3d8fc679cdcff6791cf1116991c9f6826862413
dlgt7 <102397160+dlgt7@users.noreply.github.com>
2022-10-08 16:55:20 +0800

    Update Proxy.java

--- app/src/main/java/com/github/catvod/spider/Proxy.java
+++ app/src/main/java/com/github/catvod/spider/Proxy.java
@@ -59,7 +59,9 @@ public class Proxy extends Spider {
                 return PushAgent.vod(params);
             } else if (what.equals("kmys")) {
                 return Kmys.vod(params);
-            }  else if (what.equals("ali")) {
+            } else if (what.equals("kansj")) {
+                return kansj.vod(params);
+            } else if (what.equals("ali")) {
                 return PushAgent.vod(params);
             } else if (what.equals("czspp")) {
                 return Czsapp.loadsub(params.get("url"));
